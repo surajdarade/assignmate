a8c591310bad0128f9a3fd86d54b4552ee76d902
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-12-13 19:05:46 +0530

    display nothing until nothing is loaded

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,8 +4,19 @@ import { AppSidebar } from "@/components/app-sidebar";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { useUser } from "@clerk/nextjs";
 
-export default function HomeLayout({ children }: { children: React.ReactNode }) {
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isLoaded, isSignedIn } = useUser();
+
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
+
   return (
     <SidebarProvider>
       <div className="flex min-h-screen">
